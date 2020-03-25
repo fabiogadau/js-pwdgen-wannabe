cc46e6ee2d3cc44a6cc151c63aa012d9083d26a0
Fabio Gadau <fabiogadau92@gmail.com>
2020-03-25 15:35:19 +0100

    ok

--- script/main.js
+++ script/main.js
@@ -5,15 +5,24 @@ var name = prompt('What\'s your name?');
 
 console.log(name);
 
+
 // 2. Aks for user's surname
 var surname = prompt('What\'s your surname?');
 
 console.log(surname);
+
 
 // 3. Ask for user's favourite color
 var favouriteColor = prompt('What\'s your favourite color?');
 
 console.log(favouriteColor);
 
-// 4. Enter user's informations inside markup
-document.getElementById('password').innerHTML = name + surname + favouriteColor;
+
+// 4. Ask for user's year of birth
+var birthYear = prompt('What\'s your year of birth?');
+
+console.log(birthYear);
+
+
+// 5. Enter user's informations inside markup
+document.getElementById('password').innerHTML = name + surname + favouriteColor + birthYear + '20';
