08dfc2ca85a4e7d3a19f6918851d51a31690607f
Fabio Gadau <fabiogadau92@gmail.com>
2020-03-25 15:30:31 +0100

    ok

--- script/main.js
+++ script/main.js
@@ -1,9 +1,19 @@
 console.log('JS is working!');
 
 // 1. Ask for user's name
+var name = prompt('What\'s your name?');
+
+console.log(name);
 
 // 2. Aks for user's surname
+var surname = prompt('What\'s your surname?');
+
+console.log(surname);
 
 // 3. Ask for user's favourite color
+var favouriteColor = prompt('What\'s your favourite color?');
+
+console.log(favouriteColor);
 
 // 4. Enter user's informations inside markup
+document.getElementById('password').innerHTML = name + surname + favouriteColor;
